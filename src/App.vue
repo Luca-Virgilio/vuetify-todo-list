fcaa6c6ae82d6b42a52c6051012e1d2e333113c4
Luca <luca.virgilio05@gmail.com>
2020-11-03 11:00:52 +0100

    Create navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,35 @@
 <template>
   <v-app>
     <v-card class=" overflow-hidden flex">
-      <v-system-bar color="deep-purple darken-3"></v-system-bar>
-
-      <v-app-bar color="deep-purple accent-4" dark prominent>
-        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-toolbar-title>Todo List</v-toolbar-title>
-      </v-app-bar>
-
-      <v-navigation-drawer v-model="drawer" absolute top temporary>
-        <v-list-item>
-          <v-list-item-content>
-            <v-list-item-title class="title"> Application </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-divider></v-divider>
-        <v-list nav dense>
-          <v-list-item-group
-            v-model="group"
-            active-class="deep-purple--text text--accent-4"
-            v-for="view in views"
-            :key="view.name"
-          >
-            <v-list-item router :to="view.route">
-              <v-list-item-icon>
-                <v-icon>{{ view.icon }}</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>{{ view.name }}</v-list-item-title>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
-      </v-navigation-drawer>
+      <Navbar/>
       <div><router-view /></div>
     </v-card>
   </v-app>
 </template>
 
 <script>
-// import Home from "./views/Home";
+import Navbar from "./components/Navbar";
 // import About from "./views/About";
 
 export default {
   name: "App",
   components: {
-    // Home,
-    // About
+    Navbar
   },
-  data: () => ({
-    drawer: false,
-    group: null,
-    views: [
-      { name: "Home", icon: "mdi-view-dashboard", route: "/" },
-      { name: "About", icon: "mdi-help-box", route: "/about" },
-    ],
-  }),
+  // data: () => ({
+  //   drawer: false,
+  //   group: null,
+  //   views: [
+  //     { name: "Home", icon: "mdi-view-dashboard", route: "/" },
+  //     { name: "About", icon: "mdi-help-box", route: "/about" },
+  //   ],
+  // }),
 
-  watch: {
-    group() {
-      this.drawer = false;
-    },
-  },
+  // watch: {
+  //   group() {
+  //     this.drawer = false;
+  //   },
+  // },
 };
 </script>
 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="navbar-container">
+    <v-toolbar flat dense app dark color="deep-purple darken-3">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase"> <span class="font-weight-light">Todo List</span></v-toolbar-title>
+    </v-toolbar>
+   
+    <!-- <v-system-bar color="deep-purple darken-3"></v-system-bar>
+
+    <v-app-bar color="deep-purple accent-4" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Todo List</v-toolbar-title>
+    </v-app-bar> -->
+
+    <v-navigation-drawer v-model="drawer" absolute top temporary>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title"> Application </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+          v-for="view in views"
+          :key="view.name"
+        >
+          <v-list-item router :to="view.route">
+            <v-list-item-icon>
+              <v-icon>{{ view.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>{{ view.name }}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data: () => ({
+    drawer: false,
+    group: null,
+    views: [
+      { name: "Home", icon: "mdi-view-dashboard", route: "/" },
+      { name: "About", icon: "mdi-help-box", route: "/about" },
+    ],
+  }),
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
+};
+</script>
