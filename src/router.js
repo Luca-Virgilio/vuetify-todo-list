df52617bc35a4f1ba67a3f707c42e5fd6f3ce888
Luca <luca.virgilio05@gmail.com>
2020-11-05 11:13:44 +0100

    lazy-load vews

--- src/router.js
+++ src/router.js
@@ -1,30 +1,32 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Home from './views/Home';
-import Store from './views/Store';
-import About from './views/About';
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home";
 
 Vue.use(Router);
 
 export default new Router({
-    mode:'history',
-    base:process.env.BASE_URL,
-    routes : [
-        {
-            path: '/',
-            name:'home',
-            component: Home
-        },
-        {
-            path: '/store',
-            name:'store',
-            component: Store
-        },
-        {
-            path: '/about',
-            name:'about',
-            component: About
-        }
-    ]
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+    {
+      path: "/store",
+      name: "store",
+      //lazy-loaded code splitting
+      //this generates separate chunk for this route.
+      // which is lazy-loaded when the route is visited
+      component: () =>
+        import(/* webpackChunkName: "store" */ "./views/Store.vue"),
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue"),
+    },
+  ],
 });
-
